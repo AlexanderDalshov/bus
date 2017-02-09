cfe42ced22dfc7e2d47b765cec9673649b63557f
Alexander Dalshov <adalshov@navionics.com>
2017-02-10 01:12:06 +0300

    initial project structure
    	new file:   .clang-format
    	new file:   .dir-locals.el
    	new file:   .gitignore
    	new file:   .projectile
    	new file:   CMakeLists.txt
    	new file:   src/main.cpp

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((cmake-ide-project-dir . "/Users/mac/Workspace/uv/ebus")
+         (cmake-ide-build-dir . "/Users/mac/Workspace/uv/ebus/build")
+	 (projectile-project-name . "ebus")
+	 (projectile-project-run-cmd . "./build/ebus")
+	 )))
